f9d893c11a4893ba41ab7c8f32f58ee5d1c118eb
NathanosDev <contact@nathanos.dev>
2025-01-28 14:58:33 +0100

    fix: add missing merge method input

--- actions/approve-pr/src/main.ts
+++ actions/approve-pr/src/main.ts
@@ -3,12 +3,12 @@ import * as github from '@actions/github';
 import { getInput, getNumberInput } from '@dfinity/action-utils';
 
 import { approvePullRequest } from './approve-pull-request';
-import { enableAutoMerge } from './enable-auto-merge';
+import { enableAutoMerge, MergeMethod } from './enable-auto-merge';
 
 export async function run(): Promise<void> {
   try {
     const token = getInput('token');
-    const mergeMethod = getInput('merge_method');
+    const mergeMethod = getMergeMethod();
     const pullRequestNumber = getNumberInput('pull_request_number');
 
     const octokit = github.getOctokit(token);
@@ -33,5 +33,22 @@ export async function run(): Promise<void> {
     if (error instanceof Error) {
       core.setFailed(error.message);
     }
+  }
+}
+
+function getMergeMethod(): MergeMethod {
+  const mergeMethod = getInput('merge_method').toLowerCase();
+
+  switch (mergeMethod) {
+    case 'merge':
+      return MergeMethod.Merge;
+    case 'squash':
+      return MergeMethod.Squash;
+    case 'rebase':
+      return MergeMethod.Rebase;
+    default:
+      throw new Error(
+        `Invalid merge method: ${mergeMethod}. Supported values are "merge", "squash", and "rebase".`,
+      );
   }
 }
